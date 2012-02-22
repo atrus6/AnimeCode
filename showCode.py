524eb1a12e52a8668dc64793dc7b9e4436ed8b43
Tim Butram <atrus6@gmail.com>
2012-02-22 18:28:25 -0500

    Removed non-working python stuff, working on a java section first.

--- showCode.py
+++ showCode.py
@@ -1,25 +1,29 @@
 def startPython(name):
-    work = open(name, 'r')
-    output = open(name + '.ShowCode', 'w')
+  work = open(name, 'r')
+  output = open(name + '.ShowCode', 'w')
 
-    for line in work:
-        line.expandtabs(4)
-        before = len(line)
-        after = len(line.lstrip())
 
-        prefix = ' '*(before - after)
-        
-        output.write('\n' + line)
-        line = line.rstrip()
-        line = line.replace('"', '\\"')
-        line = line.replace("'", "\\'")
-        
-        if line.isspace() == False or len(line) != 0:
-            mys = prefix + 'print \'' + line + '\''
-            if mys.find("print ''") == -1:
-                if line.endswith(':') == False:
-                    output.write(mys)
-                else:
-                    output.write('    ' + mys)
+def java(input_file, output_file):
+
+  buf = '';
+  in_method = False
+  in_comment = False
+  num_opens = 0;
+
+  for line in input_file:
+    if num_opens < 2:
+      buf += line + '\\n'
+      if line.find('{'):
+        num_opens += 1
+      elif line.find('}')
+        num_opens -= 1
+    else
+      if not in_comment:
+        output.write('System.out.println("' + buf + '");')
+      else:
+        #Check to see if we are still in a comment.
+        if line.find('*/') > -1: #Not in comment.
+          in_comment = False
+
 
 startPython('test.py')
